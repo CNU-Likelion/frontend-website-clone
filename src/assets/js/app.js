332e2f66f2194e33f7c79508e900e4f75a40123a
rlcz1 <rbf0311@gmail.com>
2024-03-04 21:45:15 +0900

    feat: slide

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,74 @@
 class App {
   constructor() {
     this.addEvent();
+    this.slide();
   }
 
   addEvent() {
-    $(document).on("click", ".qna__item", this.qnaToggle)
+    $(document).on("click", ".qna__item", this.qnaToggle);
+  }
+
+  slide() {
+    const $images = $(".gallery__image");
+    const $texts = $(".gallery__text");
+    const $buttons = $(".gallery__button");
+
+    let currentSlide = 0;
+
+    $images.eq(0).show();
+    $texts.eq(0).show();
+
+    // const showNextSlide = () => {
+    //   $images.eq(currentSlide).fadeOut(1000);
+    //   $texts.eq(currentSlide).fadeOut(1000);
+    //   $buttons.eq(currentSlide).removeClass("gallery__button--active");
+  
+    //   currentSlide = (currentSlide + 1) % $images.length;
+
+    //   $images.eq(currentSlide).fadeIn(1000);
+    //   $texts.eq(currentSlide).fadeIn(1000);
+    //   $buttons.eq(currentSlide).addClass("gallery__button--active");
+    // }
+
+    // setInterval(showNextSlide, 3000);
+
+    // $(document).on("click", ".gallery__button", (e) => {
+    //   const $this = $(e.currentTarget);
+    //   const index = $this.index();
+
+    //   $images.eq(currentSlide).fadeOut(1000);
+    //   $texts.eq(currentSlide).fadeOut(1000);
+    //   $buttons.eq(currentSlide).removeClass("gallery__button--active");
+
+    //   currentSlide = index;
+
+    //   $images.eq(currentSlide).fadeIn(1000);
+    //   $texts.eq(currentSlide).fadeIn(1000);
+    //   $buttons.eq(currentSlide).addClass("gallery__button--active");
+    // });
+
+    const showSlide = (index) => {
+      $images.eq(currentSlide).fadeOut(1000);
+      $texts.eq(currentSlide).fadeOut(1000);
+      $buttons.eq(currentSlide).removeClass("gallery__button--active");
+
+      currentSlide = index;
+
+      $images.eq(currentSlide).fadeIn(1000);
+      $texts.eq(currentSlide).fadeIn(1000);
+      $buttons.eq(currentSlide).addClass("gallery__button--active");
+    }
+
+    setInterval(() => {
+      showSlide((currentSlide + 1) % $images.length);
+    }, 3000);
+
+    $(document).on("click", ".gallery__button", (e) => {
+      const $this = $(e.currentTarget);
+      const index = $this.index();
+
+      showSlide(index);
+    });
   }
 
   qnaToggle = (e) => {
